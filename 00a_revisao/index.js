d600c841ca908be9d488cb142d984119e6f92ce3
cintiafumi <cintiafumi@gmail.com>
2019-10-28 13:31:20 -0300

    resolvendo exercicio de revisao

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,26 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+const body = document.body;
+const botao = document.getElementById('trocaCor');
+
+body.style.backgroundColor = 'purple';
+// 'rgb(____,_____,____)'
+botao.addEventListener('click', mudaCor);
+
+function geraInteiro() {
+  const n = parseInt(Math.random() * 255);
+  return n;
+}
+
+function mudaCor() {
+  const r = geraInteiro();
+  const g = geraInteiro();
+  const b = geraInteiro();
+  body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+}
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -8,7 +8,20 @@
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
+// [x] remover repetição de código
+// [x] anonymous function
 // [] arrow function
 // [] template string
+
+const body = document.body;
+const botao = document.getElementById('trocaCor');
+
+body.style.backgroundColor = 'purple';
+botao.addEventListener('click', () => {
+  const r = geraInteiro(255);
+  const g = geraInteiro(255);
+  const b = geraInteiro(255);
+  body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+});
+
+geraInteiro = num => parseInt(Math.random() * num);
