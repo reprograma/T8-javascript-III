1abe9f4d23a133f030b357b2d5ddf1937968880e
cintiafumi <cintiafumi@gmail.com>
2019-10-30 12:43:16 -0300

    comentários dos exercícios

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -1,10 +1,3 @@
-// [] instanciar XMLHttpRequest()
-// [] o que fazer depois que receber a resposta
-// [] fazer a requisição com open() e send()
-// [] tratar response
-
-
-
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
--- 01c_ajax_json/index.js
+++ 01c_ajax_json/index.js
@@ -1,14 +1,2 @@
-// [] instanciar XMLHttpRequest()
-// [] o que fazer depois que receber a resposta
-// [] fazer a requisição com open() e send()
-// [] tratar response
-
-
-// [] iterar sobre o array e mostrar as três imagens usando for()
-
-// [] iterar sobre o array e mostrar as três imagens usando for...of
-
-// [] iterar sobre o array e mostrar as três imagens usando forEach
-
 // [] encapsular em Promise()
 // [] tratar erro .then() e .catch()
