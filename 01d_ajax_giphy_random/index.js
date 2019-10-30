b7aa9517b96156399986df9489b0b9c6e44676a0
cintiafumi <cintiafumi@gmail.com>
2019-10-30 12:37:16 -0300

    ajax giphy api

--- 01d_ajax_giphy_random/index.js
+++ 01d_ajax_giphy_random/index.js
@@ -1,7 +1,23 @@
-// fetch da imagem
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] usar o método fetch() para a url da API Giphy
-// [] transformar o dado recebido
-// [] apresentar <img>
-// [] usar async/await, then(), .catch(), arrow function
+// XMLHttpRequest do gif
+
+const quero = 'rainbow'
+const urlRequest = `http://api.giphy.com/v1/gifs/search?q=${quero}&api_key=dc6zaTOxFJmzC&limit=3&offset=0`
+
+const request = new XMLHttpRequest()
+request.onreadystatechange = mostraGif;
+request.open('GET', urlRequest);
+request.send();
+
+function mostraGif() {
+  if (request.readyState === 4 && request.status === 200) {
+    const response = request.response;
+    const json = JSON.parse(response);
+
+    const body = document.body;
+    body.innerHTML = `
+      <img src=${json.data[0].images.original.url}>
+      <img src=${json.data[1].images.original.url}>
+      <img src=${json.data[2].images.original.url}>
+   `
+  }
+}
