2144711df467369fd527b6679128f0de4eb81e98
cintiafumi <cintiafumi@gmail.com>
2019-11-01 07:50:47 -0300

    exercicio api github

---
+++ github_api/index.js
@@ -0,0 +1,3 @@
+// documentacao da api do github  https://developer.github.com/v3/
+// requisição na api pela url https://api.github.com
+// fazer GET na rota /users com seu user ex: get em https://api.github.com/users/reprograma
